9046c53a8d1c559ef84aec6eefb4f73467eecf69
Artem Avetian <artemiustas@gmail.com>
2025-12-10 23:43:20 +0200

    add ouath support

--- tableau_jdbc_connector/connectionProperties.js
+++ tableau_jdbc_connector/connectionProperties.js
@@ -1,24 +1,45 @@
 (function propertiesbuilder(attr) {
 
     var props = {};
-    props["user"] = attr[connectionHelper.attributeUsername];
-    props["password"] = attr[connectionHelper.attributePassword];
 
-    props["use_server_time_zone_for_dates"] = 1;
+    // Determine authentication method: username/password or OAuth
+    var auth = attr["authentication"] || "auth-user-pass";
+
+    if (auth === "oauth") {
+        // OAuth flow: Tableau puts the access token into ACCESSTOKEN
+        // Property name "access_token" may need to be adjusted to what
+        // your ClickHouse JDBC driver expects (e.g. accessToken / token).
+        if (attr["ACCESSTOKEN"]) {
+            props["access_token"] = attr["ACCESSTOKEN"];
+        }
+
+        // Optionally still pass username if it exists (depends on your backend)
+        if (attr[connectionHelper.attributeUsername]) {
+            props["user"] = attr[connectionHelper.attributeUsername];
+        }
+
+        // Do NOT set password in OAuth mode
+    } else {
+        // Classic username/password authentication
+        props["user"] = attr[connectionHelper.attributeUsername];
+        props["password"] = attr[connectionHelper.attributePassword];
+    }
+
+    // Common properties
     props["use_server_time_zone_for_dates"] = 1;
     props["socket_timeout"] = 300000;
 
-    if ( attr["v-timeout"] > "" && !isNaN(attr["v-timeout"]) )
-    {
+    if (attr["v-timeout"] > "" && !isNaN(attr["v-timeout"])) {
         props["socket_timeout"] = attr["v-timeout"];
     }
 
-    if (attr[connectionHelper.attributeSSLMode] == "require" || attr[connectionHelper.attributePort] == "8443" || attr[connectionHelper.attributePort] == "443")
-    {
+    if (attr[connectionHelper.attributeSSLMode] == "require" ||
+        attr[connectionHelper.attributePort] == "8443" ||
+        attr[connectionHelper.attributePort] == "443") {
+
         props["ssl"] = "true";
         props["sslmode"] = "STRICT";
     }
 
     return props;
-}
-)
+})
